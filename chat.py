ad5d25f7a8ed9453b0c3fd98f4f80b99e72a2f33
burgesszhu <burgesszhu@users.noreply.github.com>
2017-03-15 18:09:32 +0800

    v1.0
    first version

---
+++ chat.py
@@ -0,0 +1,8 @@
+__author__ = 'efiklnv'
+
+userName = raw_input("What is your name?")
+message = raw_input("Enter a message:")
+
+while message != 'exit' :
+    print userName + " : "+ message
+    message = raw_input("Enter a message:")
